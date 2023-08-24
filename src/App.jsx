fd312fc4620148320ba486c8b63940bdf3544cab
MarcoGael <marcotecchi2@gmail.com>
2023-08-24 10:40:41 -0600

    Add: Initial project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  const name = 'Marco';
+  const age = 19;
+
+  const Hello = (props) => {
+    return (
+      <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Miri' age={10 + 7}/>
+      <Hello name={name} age={age}/>
+    </div>
+  )
+}
+
+export default App
